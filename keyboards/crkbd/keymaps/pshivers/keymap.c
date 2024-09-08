01e7107a4a0184696cb2ff21d584ee916d2c4a21
PShivers <paulsshivers@gmail.com>
2024-09-08 15:04:55 -0400

    fixed letter

--- keyboards/crkbd/keymaps/pshivers/keymap.c
+++ keyboards/crkbd/keymaps/pshivers/keymap.c
@@ -20,11 +20,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
     //,----------------------------------------------------------------------.                                ,-----------------------------------------------------.
-        KC_TAB,    KC_W,    KC_L,    KC_Y,    KC_P,    KC_B,                                                   KC_Z,    KC_F,    KC_O,    KC_U,   KC_SCLN,  KC_DEL,
+        KC_TAB,    KC_W,    KC_L,    KC_Y,    KC_P,    KC_B,                                                  KC_Z,    KC_F,    KC_O,    KC_U,   KC_SCLN,  KC_DEL,
     //|--------+--------+--------+---------+-----------------------+--------|                                |--------+--------+--------+--------+--------+--------|
-        KC_BSPC,    KC_C,    KC_R,    KC_S,   MT(MOD_LSFT, KC_T),    KC_G,                                    KC_M,    MT(MOD_RSFT, KC_T),    KC_E,    KC_I, KC_A, KC_QUOT,
+        KC_BSPC,    KC_C,    KC_R,    KC_S,   MT(MOD_LSFT, KC_T),    KC_G,                                    KC_M,    MT(MOD_RSFT, KC_N),    KC_E,    KC_I, KC_A, KC_QUOT,
     //|--------+--------+--------+---------+-----------------------+--------|                                |--------+--------+--------+--------+--------+--------|
-        KC_LSFT,    KC_Q,    KC_J,    KC_V,    KC_D,    KC_K,                                                  KC_X,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+        KC_LSFT,    KC_Q,    KC_J,    KC_V,    KC_D,    KC_K,                                                 KC_X,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
     //|--------+--------+--------+--------+--------+--------+-----------------------------|     |-------------------+-------+--------+--------+--------+--------|
                                             KC_LALT,  LT(MO(1), KC_ENT),  LCTL_T(KC_SPC),         LT(MO(3), KC_ENT),   MO(2), KC_LGUI
                                         //`-----------------------------------------------'     `--------------------+-------+--------+
