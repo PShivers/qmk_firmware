dc1e40ae2935522c26c470590b03c5f0d778ac81
PShivers <paulsshivers@gmail.com>
2024-09-08 15:01:52 -0400

    updated crkbd

--- keyboards/crkbd/keymaps/pshivers/config.h
+++ keyboards/crkbd/keymaps/pshivers/config.h
@@ -17,9 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #pragma once
-// trying to fix a bug in the firmware https://github.com/foostan/crkbd/issues/170 
 
 // #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 180
@@ -38,6 +36,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_MAX_SPEED 6
 #undef MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX 64
-
-// #define USE_SERIAL
-// #define MASTER_LEFT
