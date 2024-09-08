b6136659fa842a84586d76f1c1416b19abb41db7
PShivers <paulsshivers@gmail.com>
2024-09-08 00:47:13 -0400

    reverting to working version

--- keyboards/crkbd/keymaps/pshivers/config.h
+++ keyboards/crkbd/keymaps/pshivers/config.h
@@ -1,63 +1,40 @@
-/* Copyright 2015-2021 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
-#ifdef AUDIO_ENABLE
-#    define STARTUP_SONG SONG(PLANCK_SOUND)
-// #define STARTUP_SONG SONG(NO_SOUND)
+// #define USE_MATRIX_I2C
 
-#    define DEFAULT_LAYER_SONGS \
-        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
-#endif
+/* Select hand configuration */
 
-/*
- * MIDI options
- */
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
 
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
-
-#define MIDI_BASIC
-
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-// #define MIDI_ADVANCED
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-// #define MIDI_TONE_KEYCODE_OCTAVES 2
-
-// Most tactile encoders have detents every 4 stages
-#define ENCODER_RESOLUTION 4
-
-#define TAPPING_TERM 180
-
-#define TAPPING_TERM_PER_KEY
-// This allows for tap/hold delay to be defined in the keymap.c
-// #define TAPPING_TERM_PER_KEY
+// #define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 200
 // this is a potential solution to my space/ctrl issue pulled from: https://www.reddit.com/r/olkb/comments/8tbp1r/qmk_modtap_registering_mod_within_tapping_term/
-// #define IGNORE_MOD_TAP_INTERRUPT
-// this is now the default so no longer needs inclusion. leaving in for now in case
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
 
-// Mouse key speed and acceleration.
+// Mouse stuff
 #undef MOUSEKEY_DELAY
 #define MOUSEKEY_DELAY 0
 #undef MOUSEKEY_INTERVAL
@@ -69,4 +46,4 @@
 #undef MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX 64
 
-#define RGBLIGHT_ANIMATIONS
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
--- keyboards/crkbd/keymaps/pshivers/keymap.c
+++ keyboards/crkbd/keymaps/pshivers/keymap.c
@@ -1,59 +1,54 @@
+// /*
+// Copyright 2019 @foostan
+// Copyright 2020 Drashna Jaelre <@drashna>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// */
+
 #include QMK_KEYBOARD_H
-#include <stdint.h>
 
-#ifndef SAFE_RANGE
-#    define SAFE_RANGE 0x1000
-#endif
-
-#define _CANARY 0
-#define _NUMFN 1
-#define _SYMBOL 2
-#define _NAV 3
+#define _LAYER0 0
+#define _LAYER1 1
+#define _LAYER2 2
+#define _LAYER3 3
+#define _LAYER4 4
 
 enum custom_keycodes {
-    CANARY = SAFE_RANGE,
-    NUMFN,
-    SYMBOL,
-    NAV,
-};
-
-// Define the combo keys
-enum combo_events { COMBO_CAPSLOCK, COMBO_LENGTH };
-
-// Specify the keys for the combo
-const uint16_t PROGMEM capslock_combo[] = {KC_LSFT, KC_RSFT, COMBO_END};
-
-// Define the combo actions
-combo_t key_combos[COMBO_LENGTH] = {
-    [COMBO_CAPSLOCK] = COMBO(capslock_combo, KC_CAPS)
+    LAYER0 = SAFE_RANGE,
+    LAYER1,
+    LAYER2,
+    LAYER3,
+    LAYER4,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_CANARY] = LAYOUT(
-        KC_TAB, KC_W, KC_L, KC_Y, KC_P, KC_B,                       KC_Z, KC_F, KC_O, KC_U, KC_SCLN, KC_DEL,
-        KC_BSPC, KC_C, KC_R, KC_S, LSFT_T(KC_T), MEH_T(KC_G),       KC_M, RSFT_T(KC_N), KC_E, KC_I, KC_A, KC_QUOT,
-        KC_LSFT, KC_Q, KC_J, KC_V, KC_D, KC_K,                      KC_X, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        KC_LALT, LT(MO(_NUMFN), KC_ENT), LCTL_T(KC_SPC),            LT(MO(_NAV), KC_ENT), LT(MO(_SYMBOL), KC_DEL), KC_LGUI
-    ),
 
-    [_NUMFN] = LAYOUT(
-        KC_ESC, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO,               KC_NO, KC_7, KC_8, KC_9, KC_MEDIA_SELECT, KC_DEL,
-        KC_BSPC, KC_F5, KC_F6, KC_F7, KC_F8, LCTL(KC_GRV),          KC_NO, KC_4, KC_5, KC_6, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,
-        KC_CAPS, KC_F1, KC_F2, KC_F3, KC_F4, LCTL(KC_SLSH),         KC_NO, KC_1, KC_2, KC_3, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,
-        LCTL(LGUI(KC_LEFT)), KC_TRNS, LCTL(LGUI(KC_RIGHT)),         KC_TRNS, KC_0, KC_TRNS
-    ),  
+    [_LAYER0] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B,                     KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_DEL, 
+                       KC_BSPC, KC_A, KC_R, KC_S, LSFT_T(KC_T), MEH_T(KC_G),     KC_M, RSFT_T(KC_N), KC_E, KC_I, KC_O, KC_QUOT, 
+                       KC_LGUI, KC_Z, KC_X, KC_C, KC_D, KC_V,                    KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, TO(1),
+                       KC_LALT, LT(MO(2), KC_ENT), LCTL_T(KC_SPC),               LT(MO(4), KC_ENT), MO(3), MO(2)),
 
-    [_SYMBOL] = LAYOUT(
-        KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_NO, KC_LBRC, KC_RBRC, KC_MINS, KC_SCLN, KC_DEL,
-        KC_BSPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_LPRN, KC_RPRN, KC_EQL, KC_GT, KC_GRV,
-        KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_LCBR, KC_RCBR, KC_DOT, KC_BSLS, KC_RSFT,
-        KC_NO, KC_NO, KC_NO,                                        KC_NO, KC_TRNS, KC_NO
-    ),
+    [_LAYER1] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_BSPC, KC_A, KC_S, KC_D, LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TO(0), KC_LALT, LT(MO(2), KC_ENT), LCTL_T(KC_SPC), LT(MO(4), KC_ENT), MO(3), MO(2)),
 
-    [_NAV] = LAYOUT(
-        KC_NO, KC_NO, KC_WH_U, KC_MS_U, KC_WH_D, KC_NO,             KC_NO, KC_PGDN, KC_UP, KC_PGUP, KC_ACL0, KC_ACL1,
-        KC_NO, KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,         LCTL(KC_LEFT), KC_LEFT, KC_DOWN, KC_RGHT, LCTL(KC_RGHT), KC_ACL2,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_HOME, KC_NO, KC_END, KC_NO, LCA(KC_RGHT),
-        KC_BTN2, KC_BTN3, KC_BTN1,                                  KC_TRNS, KC_NO, KC_NO
-    ),
+    [_LAYER2] = LAYOUT(KC_ESC, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_MEDIA_SELECT, KC_DEL, KC_BSPC, KC_F5, KC_F6, KC_F7, KC_F8, LCTL(KC_GRV), KC_NO, KC_4, KC_5, KC_6, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_CAPS, KC_F1, KC_F2, KC_F3, KC_F4, LCTL(KC_SLSH), KC_NO, KC_1, KC_2, KC_3, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, LCTL(LGUI(KC_LEFT)), KC_TRNS, LCTL(LGUI(KC_RIGHT)), KC_NO, KC_0, KC_TRNS),
+
+    [_LAYER3] = LAYOUT(KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_MINS, KC_SCLN, KC_DEL, KC_BSPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LPRN, KC_RPRN, KC_EQL, KC_GT, KC_GRV, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCBR, KC_RCBR, KC_DOT, KC_BSLS, KC_RSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO),
+
+    [_LAYER4] = LAYOUT(KC_NO, KC_NO, KC_WH_U, KC_MS_U, KC_WH_D, KC_NO,          KC_NO, KC_PGDN, KC_UP, KC_PGUP, KC_ACL0, KC_ACL1, 
+                       KC_NO, KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,      LCTL(KC_LEFT), KC_LEFT, KC_DOWN, KC_RGHT, LCTL(KC_RGHT), KC_ACL2, 
+                       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_HOME, KC_NO, KC_END, KC_NO, LCA(KC_RGHT),
+                                         KC_BTN2, KC_BTN3, KC_BTN1, KC_TRNS, KC_NO, KC_NO
+                      )
+
 };
